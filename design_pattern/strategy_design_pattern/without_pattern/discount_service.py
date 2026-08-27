24750e70b8125fd96c213684d0972d9de1981862
Prarinam Deria <prarinamderia2000@gmail.com>
2026-08-27 14:33:25 +0530

    strtegy design pattern without pattern
    Signed-off-by: Prarinam Deria <prarinamderia2000@gmail.com>

---
+++ design_pattern/strategy_design_pattern/without_pattern/discount_service.py
@@ -0,0 +1,8 @@
+class DiscountService:
+    def calculate_discount(self, discount_type: str):
+        if discount_type == "Diwali":
+            print("Applying diwali discount of 20%")
+        elif discount_type == "First Order":
+            print("Applying first order discount of 15%")
+        else:
+            print("No discount applied")
---
+++ design_pattern/strategy_design_pattern/without_pattern/main.py
@@ -0,0 +1,5 @@
+from discount_service import DiscountService
+
+discount_service = DiscountService()
+discount_service.calculate_discount("Diwali")
+discount_service.calculate_discount("second order")
